569008d7b5b4d49f9d1eb28ed6ce0564e15c5fd1
rcrs3 <rcrs3@cin.ufpe.br>
2017-10-15 18:57:22 -0300

    Chat working

--- public/js.js
+++ public/js.js
@@ -1,35 +1,85 @@
 window.onload = function() {
-  var socket = io.connect("http://localhost:80");
-  var DOMStrings = {
-    usernameBlock: ".username_block",
-    chat: ".chat",
-    nameArea: ".name",
-    submit: ".submit",
-    textArea: ".text-area",
-    messages: ".chat-messages",
-    sendBtn: ".send-text"
-  }
-
-  document.querySelector(DOMStrings.submit).addEventListener("submit", function(e) {
-    e.preventDefault();
-    document.querySelector(DOMStrings.chat).removeAttribute("hidden");
-    document.querySelector(DOMStrings.usernameBlock).setAttributeNode(document.createAttribute("hidden"));
-  });
-
-  var sendText = function() {
-    var text = document.querySelector(DOMStrings.textArea).value;
-    var time = new Date();
-    var html = '<li class="self"><div class="msg"><span><p>' + text + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>';
-    document.querySelector(DOMStrings.textArea).value = '';
-    document.querySelector(DOMStrings.messages).insertAdjacentHTML("beforeend", html);
-    socket.emit("send", text);
-  }
-
-  document.querySelector(DOMStrings.textArea).addEventListener("keydown", function(e) {
-    if(e.which === 13 || e.keyCode === 13) {
-      sendText();
+  var variaveis = (function() {
+    var socket = io.connect("http://localhost:80");
+    var connected = false;
+    var name = "";
+    var DOMStrings = {
+      usernameBlock: ".username_block",
+      chat: ".chat",
+      nameArea: ".name",
+      submit: ".submit",
+      textArea: ".text-area",
+      messages: ".chat-messages",
+      sendBtn: ".send-text"
     }
-  });
 
-  document.querySelector(DOMStrings.sendBtn).addEventListener("click", sendText);
+    return {
+      getDOMStrings: function() {
+        return DOMStrings;
+      },
+      getUsername: function() {
+        return name;
+      },
+      getState: function() {
+        return connected;
+      },
+      getSocket: function() {
+        return socket;
+      },
+      setState: function(state) {
+        connected = state;
+      },
+      setUsername: function(n) {
+        name = n;
+      }
+    }
+  })();
+  AppController = (function(variaveis) {
+    document.querySelector(variaveis.getDOMStrings().submit).addEventListener("submit", function(e) {
+      e.preventDefault();
+      document.querySelector(variaveis.getDOMStrings().chat).removeAttribute("hidden");
+      document.querySelector(variaveis.getDOMStrings().usernameBlock).setAttributeNode(document.createAttribute("hidden"));
+
+      variaveis.setUsername(document.querySelector(variaveis.getDOMStrings().nameArea).value);
+      var time = new Date();
+
+      variaveis.setState(true);
+      variaveis.getSocket().emit("join", variaveis.getUsername());
+    });
+
+    var addLi = function(name, text) {
+      var time = new Date();
+      var html = '<li class="self"><div class="msg"><span>' + name + ':</span><p>' + text + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>';
+      document.querySelector(variaveis.getDOMStrings().messages).insertAdjacentHTML("beforeend", html);
+    }
+
+    var sendText = function() {
+      var text = document.querySelector(variaveis.getDOMStrings().textArea).value;
+      console.log(text);
+      document.querySelector(variaveis.getDOMStrings().textArea).textContent = '';
+      console.log("NOME " + variaveis.getUsername());
+      addLi(variaveis.getUsername(), text);
+      variaveis.getSocket().emit("send", text);
+    }
+
+    document.querySelector(variaveis.getDOMStrings().textArea).addEventListener("keydown", function(e) {
+      if(e.which === 13 || e.keyCode === 13) {
+        sendText();
+      }
+    });
+
+    document.querySelector(variaveis.getDOMStrings().sendBtn).addEventListener("click", sendText);
+
+    variaveis.getSocket().on("update", function(msg) {
+      if(variaveis.getState()) {
+        document.querySelector(variaveis.getDOMStrings().textArea).insertAdjacentHTML("beforeend", '<li class="info">' + msg + '</li>');
+      }
+    });
+
+    variaveis.getSocket().on("chat", function(client, msg) {
+      if(variaveis.getState()) {
+          addLi(client, msg);
+      }
+    });
+  })(variaveis);
 }
